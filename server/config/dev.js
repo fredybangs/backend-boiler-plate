0f6f8d1a305272fb1dd8539e66f23a7c75e1cdd3
fredybangs <alfredbangura77@gmail.com>
2020-06-08 11:29:14 +0000

    GUI Added

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,5 +1,6 @@
 module.exports = {
-	mongoURI: 'mongodb://localhost/salone247',
+	mongoURI:
+		'mongodb+srv://ElRey:TNQP8uhCZ8erhAXT@backend-boilerplate-08vqw.mongodb.net/<dbname>?retryWrites=true&w=majority',
 	tokenSecret: 'khiYhhh3739ds//;^^&I448Jgs?=uT',
 };
 
