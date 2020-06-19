2b89a628ccb4e607dd9f8d9a02b1c572154de30f
fredybangs <alfredbangura77@gmail.com>
2020-06-19 14:19:36 +0000

    heroku

--- server/index.js
+++ server/index.js
@@ -58,5 +58,5 @@ if (process.env.NODE_ENV === 'production') {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-	console.log(`Server Listening on ${port}`);
+	console.log(`Server listening on ${port}`);
 });
